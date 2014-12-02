92035c92fd04bfc35b3282dcfb77d4a38824ff41
Kurt <kurtfitz@hotmail.com>
2014-12-02 15:32:21 -0800

    addfield.php

--- addfield.php
+++ addfield.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     
 //check for required fields from the form
 if ((filter_input(INPUT_POST, 'fieldname'))) {
@@ -6,11 +7,13 @@ if ((filter_input(INPUT_POST, 'fieldname'))) {
     $fieldname = filter_input(INPUT_POST, 'fieldname');
     $crop = filter_input(INPUT_POST, 'cropOption');
     $fertilizer = filter_input(INPUT_POST, 'fertOption');
+    $member_id = $_SESSION['member_id'];
+    $area = filter_input(INPUT_POST, 'fieldarea');
     
     $mysqli = mysqli_connect("localhost", "organicDBuser", "letmein", "organic");
     
-    /*$sql = "INSERT into fields values('';
-    */
+    $sql = "INSERT into fields values('','".$member_id."','".$fieldname."','".$crop."','".$area."');";
+    
     $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     header("Location: userlogin.html");
     
@@ -18,11 +21,14 @@ if ((filter_input(INPUT_POST, 'fieldname'))) {
 $displayBlock = '
        <h3> Provide Field Details Below </h3>
        <fieldset>
-       <legend></legend>
+       <legend>Add Field</legend>
        <form method="POST" action="">
            <p>Provide a name to identify your field</p>
                 Field Name:
                 <input type =\'text\' name=\'fieldname\'>
+                </br>
+                Field Area (acres):
+                <input type =\'text\' name=\'fieldarea\'>
                 </br>
                 <p>Select a Crop for this field:</p>
                 <select name="cropOption">
