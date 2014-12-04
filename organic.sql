940895beefa6f5a060bde90a9bf7cb6bec631148
Matt Delorme <matt.delorme@hotmail.com>
2014-12-03 16:33:18 -0800

    Added crop subtypes to addfield.php. Modified the DB to reflect changes. Updated viewapplications.php to show the crop name and subtype name.

--- organic.sql
+++ organic.sql
@@ -30,7 +30,7 @@ CREATE TABLE `applications` (
   `status` varchar(10) DEFAULT NULL,
   `submit_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,8 +39,33 @@ CREATE TABLE `applications` (
 
 LOCK TABLES `applications` WRITE;
 /*!40000 ALTER TABLE `applications` DISABLE KEYS */;
-INSERT INTO `applications` VALUES (8,1,'1','1','pending','2014-12-03 11:19:35'),(9,2,'2','2','pending','2014-12-03 11:27:36');
+INSERT INTO `applications` VALUES (8,1,'1','1','pending','2014-12-03 11:19:35'),(9,2,'2','2','pending','2014-12-03 11:27:36'),(10,6,'3','2','pending','2014-12-03 16:29:49');
 /*!40000 ALTER TABLE `applications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `crop_subtypes`
+--
+
+DROP TABLE IF EXISTS `crop_subtypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `crop_subtypes` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `crop_id` int(10) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `crop_subtypes`
+--
+
+LOCK TABLES `crop_subtypes` WRITE;
+/*!40000 ALTER TABLE `crop_subtypes` DISABLE KEYS */;
+INSERT INTO `crop_subtypes` VALUES (1,1,'McIntosh'),(2,1,'Spartan'),(3,1,'Gala'),(4,2,'Chinook'),(5,2,'Garnet'),(6,2,'Utah Giant'),(7,3,'Albion'),(8,3,'Bolero'),(9,3,'Camarosa'),(10,4,'Scarlet Nantes'),(11,4,'Touchon'),(12,4,'Parano');
+/*!40000 ALTER TABLE `crop_subtypes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -68,6 +93,29 @@ INSERT INTO `crops` VALUES (1,'Apples'),(2,'Cherries'),(3,'Strawberries'),(4,'Ca
 UNLOCK TABLES;
 
 --
+-- Table structure for table `fertilizer_brands`
+--
+
+DROP TABLE IF EXISTS `fertilizer_brands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fertilizer_brands` (
+  `id` int(10) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fertilizer_brands`
+--
+
+LOCK TABLES `fertilizer_brands` WRITE;
+/*!40000 ALTER TABLE `fertilizer_brands` DISABLE KEYS */;
+INSERT INTO `fertilizer_brands` VALUES (NULL,'Jerry\'s'),(NULL,'Tom\'s'),(NULL,'Super Organic'),(NULL,'Chemical Brothers');
+/*!40000 ALTER TABLE `fertilizer_brands` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `fertilizers`
 --
 
@@ -76,6 +124,7 @@ DROP TABLE IF EXISTS `fertilizers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fertilizers` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `brand_id` int(10) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
@@ -87,7 +136,7 @@ CREATE TABLE `fertilizers` (
 
 LOCK TABLES `fertilizers` WRITE;
 /*!40000 ALTER TABLE `fertilizers` DISABLE KEYS */;
-INSERT INTO `fertilizers` VALUES (1,'Jerry\'s Organic Fertilizer'),(2,'Tom\'s Blood Meal Blend'),(3,'Zinc Super'),(4,'Fast Acting Gypsum');
+INSERT INTO `fertilizers` VALUES (1,NULL,'Jerry\'s Organic Fertilizer'),(2,NULL,'Tom\'s Blood Meal Blend'),(3,NULL,'Zinc Super'),(4,NULL,'Fast Acting Gypsum');
 /*!40000 ALTER TABLE `fertilizers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,10 +151,11 @@ CREATE TABLE `fields` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `member_id` int(10) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `crop` varchar(30) DEFAULT NULL,
+  `crop` int(10) DEFAULT NULL,
+  `crop_subtype` int(10) DEFAULT NULL,
   `area` int(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +164,7 @@ CREATE TABLE `fields` (
 
 LOCK TABLES `fields` WRITE;
 /*!40000 ALTER TABLE `fields` DISABLE KEYS */;
-INSERT INTO `fields` VALUES (1,1,'Test Apple Field','1',10),(2,1,'Strawberry Field','3',12),(3,1,'Cherry Land','2',4);
+INSERT INTO `fields` VALUES (1,1,'Test Apple Field',1,1,10),(2,1,'Strawberry Field',3,7,12),(3,1,'Cherry Land',2,2,4),(5,1,'Much Macs',1,1,0),(6,1,'Savage Spartans',1,2,0);
 /*!40000 ALTER TABLE `fields` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-03 12:13:21
+-- Dump completed on 2014-12-03 16:31:17
--- viewapplications.php
+++ viewapplications.php
@@ -3,39 +3,52 @@
     include("connection.php");
     $mysqli = connectToDB();
     
-    $id = $_SESSION['member_id'];
-    $applicationsQuery = "SELECT * FROM applications WHERE field_id IN "
-	. "(SELECT id FROM fields WHERE member_id = " . $id . ") ORDER BY id DESC;";
-    $applicationsResult = mysqli_query($mysqli, $applicationsQuery) or die(mysqli_error($mysqli));
-    $display .= '<div class="content">';
-    while ($resultrow = mysqli_fetch_array($applicationsResult)) {
-	$display .= '<div class="applicationview">';
-	
-	$fieldQuery = "SELECT * FROM fields WHERE id = " . $resultrow['field_id'] . ";";
-	$fieldResult = mysqli_query($mysqli, $fieldQuery) or die(mysqli_error($mysqli));
-	$fieldrow = mysqli_fetch_array($fieldResult);
-	$display .= "<h4>" . $fieldrow['name'] . "</h4>";
-	
-	$display .= '<p>Pesticide</p>';
-	$pesticideQuery = "SELECT * FROM pesticides WHERE id = " . $resultrow['pesticide'] . ";";
-	$pesticideResult = mysqli_query($mysqli, $pesticideQuery) or die(mysqli_error($mysqli));
-	while($pesticiderow = mysqli_fetch_array($pesticideResult)) {
-	    $display .= "<li>" . $pesticiderow['name'] . "</li>";
+    if(!$_SESSION['member_id']) {
+	header("Location: login.php");
+    } else {
+	$id = $_SESSION['member_id'];
+	$applicationsQuery = "SELECT * FROM applications WHERE field_id IN "
+	    . "(SELECT id FROM fields WHERE member_id = " . $id . ") ORDER BY id DESC;";
+	$applicationsResult = mysqli_query($mysqli, $applicationsQuery) or die(mysqli_error($mysqli));
+	$display .= '<div class="content">';
+	while ($resultrow = mysqli_fetch_array($applicationsResult)) {
+	    $display .= '<div class="applicationview">';
+
+	    $fieldQuery = "SELECT * FROM fields WHERE id = " . $resultrow['field_id'] . ";";
+	    $fieldResult = mysqli_query($mysqli, $fieldQuery) or die(mysqli_error($mysqli));
+	    $fieldrow = mysqli_fetch_array($fieldResult);
+	    
+	    $cropNameQuery = "SELECT name FROM crops WHERE id = " . $fieldrow['crop'] . ";";
+	    $cropNameResult = mysqli_query($mysqli, $cropNameQuery) or die(mysqli_error($mysqli));
+	    $cropName = mysqli_fetch_array($cropNameResult)['name'];
+	    $cropSubtypeNameQuery = "SELECT name FROM crop_subtypes WHERE id = " . $fieldrow['crop_subtype'] . ";";
+	    $cropSubtypeNameResult = mysqli_query($mysqli, $cropSubtypeNameQuery) or die(mysqli_error($mysqli));
+	    $cropSubtypeName = mysqli_fetch_array($cropSubtypeNameResult)['name'];
+		    
+	    $display .= "<h4>" . $fieldrow['name'] . "</h4>";
+	    $display .= '<p>' . $cropSubtypeName . " " . $cropName . '</p>';
+		    
+	    $display .= '<p>Pesticide</p>';
+	    $pesticideQuery = "SELECT * FROM pesticides WHERE id = " . $resultrow['pesticide'] . ";";
+	    $pesticideResult = mysqli_query($mysqli, $pesticideQuery) or die(mysqli_error($mysqli));
+	    while($pesticiderow = mysqli_fetch_array($pesticideResult)) {
+		$display .= "<li>" . $pesticiderow['name'] . "</li>";
+	    }
+
+	    $display .= "<p>Fertilizer</p>";
+	    $fertilizerQuery = "SELECT * FROM fertilizers WHERE id = " . $resultrow['fertilizer'] . ";";
+	    $fertilizerResult = mysqli_query($mysqli, $fertilizerQuery) or die(mysqli_error($mysqli));
+	    while($fertilizerrow = mysqli_fetch_array($fertilizerResult)) {
+		$display .= "<li>" . $fertilizerrow['name'] . "</li>";
+	    }
+	    $display .= '<p>Status: ';
+	    $display .= $resultrow['status'] . '</p>';
+	    $display .= '<p>Submission Date: ';
+	    $display .= date("M j, Y g:i A", strtotime($resultrow['submit_date'])) . '</p>';
+	    $display .= '</div>';
 	}
-	
-	$display .= "<p>Fertilizer</p>";
-	$fertilizerQuery = "SELECT * FROM fertilizers WHERE id = " . $resultrow['fertilizer'] . ";";
-	$fertilizerResult = mysqli_query($mysqli, $fertilizerQuery) or die(mysqli_error($mysqli));
-	while($fertilizerrow = mysqli_fetch_array($fertilizerResult)) {
-	    $display .= "<li>" . $fertilizerrow['name'] . "</li>";
-	}
-	$display .= '<p>Status: ';
-	$display .= $resultrow['status'] . '</p>';
-	$display .= '<p>Submission Date: ';
-	$display .= date("M j, Y g:i A", strtotime($resultrow['submit_date'])) . '</p>';
 	$display .= '</div>';
     }
-    $display .= '</div>';
 ?>
 <?php require('head.php'); ?>
 <?php require('header.php'); ?>
